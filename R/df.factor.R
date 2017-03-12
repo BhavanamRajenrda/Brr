00711a729732f1102b10c2f5d13c69ca85ffe89d
Prabhakar <prk327@gmail.com>
2017-03-12 22:48:06 +0530

    Update df.factor.R

--- R/df.factor.R
+++ R/df.factor.R
@@ -1,9 +1,18 @@
-environment(df.factor <-
-function(x) {
+#' Factor of Character Column
+#
+#' This is an example function named 'df.factor'
+#' which convert a character column to factor.
+#' @param x the dataframe
+#' @return the dataframe character column now converted into factor
+#' @export
+
+
+df.factor <-
+  function(x) {
     for (i in 1:ncol(x)) {
       if (typeof(x[, i]) == "character") {
         x[, i] <- as.factor(x[, i])
       }
     }
     return(x)
-  })
+  }
