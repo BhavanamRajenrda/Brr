2f0291ac7774235ea2b6aa693b8e3b5160b74510
Prabhakar <prk327@gmail.com>
2017-04-17 13:53:41 +0530

    Add files via upload

---
+++ R/con_var.r
@@ -0,0 +1,25 @@
+#' Data Frame of Numeric Variable
+#
+#' This is an example function named 'con_var'
+#' which extracts numeric column of a data frame and then create a new data frame.
+#' The data frame can be given any name using name attribute, by default the name is "Numeric_VAR".
+#' @param x a data frame
+#' @param name the name of the new numeric data frame
+#' @return a dataframe of numeric column
+#' @export
+
+con_var <- function(x, name = "Numeric_VAR") {
+  cn <- 0
+  z <- data.frame()
+  for (i in seq_along(x)) {
+    if (class(x[, i]) == "numeric") {
+      cn <- trimws(sub("0,", "", paste(cn, colnames(x)[i],
+                                       sep = ",")))
+    }
+  }
+  cn <- as.vector(strsplit(cn, ","))
+  for (k in cn) {
+    z <- x[, (colnames(x) %in% k)]
+  }
+  assign(name, z, envir = .GlobalEnv)
+}
