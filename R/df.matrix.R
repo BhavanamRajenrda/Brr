b3a863ff7e35386d48a8f2ce700a3ef8a53948e5
Prabhakar <prk327@gmail.com>
2017-04-23 19:31:28 +0530

    Update df.matrix.R

--- R/df.matrix.R
+++ R/df.matrix.R
@@ -4,13 +4,12 @@
 #' which convert a factor columns levels(k-1) to different columns with value 0 and 1.
 # Then save the dataframe in to new dummy dataframe withour the initial factor column.
 #' @param x a data frame to be converted into matrix
-#' @param name the name of the new dummy dataframe
 #' @param sep the delimeter to be used for seperating column and levels
 #' @return the matrix dataframe of factor column
 #' @export
 
 df.matrix <-
-  function(x, name = "dummy", sep = ":") {
+  function(x, sep = ":") {
     cn <- 0
     y <- data.frame()
     for (i in 1:ncol(x)) {
@@ -26,6 +25,5 @@ df.matrix <-
     for (k in cn) {
       x <- x[,!(colnames(x) %in% k)]
     }
-    assign(paste(name),
-           x, envir = .GlobalEnv)
+    return(x)
   }
